3e132834e1be42cf9aca34f57d99913d6f7cf52f
Alexey <luzin.a@samberi.com>
2020-05-19 10:18:37 +1000

    modify func call

--- app/datamax_oneil/routes.py
+++ app/datamax_oneil/routes.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 from .functions import print_steakhouse_order
-from typing import List
+from .schemes import RequestStrings
+
 
 datamax_router = APIRouter()
 
 
 @datamax_router.post('/steakhouse/order', summary='Send list of strings to printer')
-def steakhouse_order(data: List[str]):
-    print_steakhouse_order(data)
+def steakhouse_order(data: RequestStrings):
+    print_steakhouse_order(data.dict()['data'])
