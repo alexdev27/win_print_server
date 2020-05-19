e71230a932379a223b3ab9d8a817767d088e711a
Alexey <luzin.a@samberi.com>
2020-05-19 10:17:25 +1000

    print_steakhouse_order is working properly

--- app/datamax_oneil/functions.py
+++ app/datamax_oneil/functions.py
@@ -1,7 +1,7 @@
-# from app.win32_printing_api.functions import start_document, apply_font, attach_text, end_document
+from app.win32_printing_api.functions import start_document, apply_font, attach_text, end_document
 
 MAX_ACCEPTABLE_CHARS = 36
-NORMAL_X_OFFSET = 80
+NORMAL_X_OFFSET = 90
 CUSTOM_X_OFFSET = 130
 START_Y_OFFSET = 100
 INCREMENTAL_Y_OFFSET = 30
@@ -15,16 +15,16 @@ arr = [
 
 
 def print_steakhouse_order(data):
-    # # # print(data)
-    # order_line = data.pop(0)
-    # doc = start_document()
-    # # # header of the order
-    # apply_font(doc, 'Consolas', 12)
-    # attach_text(order_line)
-    # # # another information
-    # apply_font(doc, 'Consolas', 9)
-    _process_strings(data)
-    # end_document()
+    # # print(data)
+    order_line = data.pop(0)
+    doc = start_document()
+    # # header of the order
+    apply_font(doc, 'Consolas', 12)
+    attach_text(doc, text=order_line)
+    # # another information
+    apply_font(doc, 'Consolas', 9)
+    _process_strings(data, doc)
+    end_document(doc)
 
 
 def _split_long_string(num_chars: int, string: str):
@@ -40,7 +40,7 @@ def _split_long_string(num_chars: int, string: str):
     return result
 
 
-def _process_strings(strings):
+def _process_strings(strings, doc):
     y_offset = START_Y_OFFSET
 
     for _str in strings:
@@ -49,8 +49,5 @@ def _process_strings(strings):
             is_need_offset = val[0]
             string = val[1]
             x_offset = CUSTOM_X_OFFSET if is_need_offset else NORMAL_X_OFFSET
-            # attach_text(x_offset, y_offset, string)
-            print(x_offset, y_offset, string)
+            attach_text(doc, x_offset, y_offset, string)
             y_offset += INCREMENTAL_Y_OFFSET
-
-print_steakhouse_order(arr)
