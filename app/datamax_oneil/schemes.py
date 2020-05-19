a936a4058309de4cb585dfcec53703c125795915
Alexey <luzin.a@samberi.com>
2020-05-19 10:18:48 +1000

    new schema

--- app/datamax_oneil/schemes.py
+++ app/datamax_oneil/schemes.py
@@ -1,1 +1,7 @@
+from typing import List
 
+from pydantic import BaseModel, Field
+
+
+class RequestStrings(BaseModel):
+    data: List[str] = Field(..., title='List of strings to print', min_items=1)
